00bdee7677e36c6661933c82ba9060a25c7cfb00
PK <prometheus.21.pk@gmail.com>
2023-03-09 17:22:48 +0530

    commit 1

---
+++ src/products/product.model.ts
@@ -0,0 +1,9 @@
+export class Product {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public price: number,
+  ) {}
+  //In eslint every line have some meaning and make it more readable
+}
